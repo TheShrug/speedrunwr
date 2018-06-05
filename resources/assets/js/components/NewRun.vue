abc7d69143722e5ab3202986ee3fb579157a2bb1
TheShrug <stewart.m.gordon@gmail.com>
2018-06-04 18:21:13 -0700

    Setting up run filters

--- resources/assets/js/components/NewRun.vue
+++ resources/assets/js/components/NewRun.vue
@@ -3,50 +3,89 @@
         <v-btn @click="getNewRun" block>
             New Run
         </v-btn>
-        <v-checkbox :label="'Autoplay'" v-model="autoPlay"></v-checkbox>
+        <v-switch
+            :label="`Autoplay ${autoPlayLabel}`"
+            v-model="autoPlay"
+        ></v-switch>
+        <v-radio-group :label="'Video Type'" v-model="videoType" row >
+            <v-radio label="All" value="0"></v-radio>
+            <v-radio label="Twitch" value="1"></v-radio>
+            <v-radio label="Youtube" value="2"></v-radio>
+        </v-radio-group>
+
+        <v-layout row wrap>
+            <v-flex xs6>
+                <v-text-field type="number" label="Min Length" v-model="minRunLength" hint="In Minutes"></v-text-field>
+            </v-flex>
+            <v-flex xs6>
+                <v-text-field type="number" label="Max Length" v-model="maxRunLength" hint="In Minutes"></v-text-field>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs6>
+                <v-menu
+                        :close-on-content-click="false"
+                        v-model="dateMenu2"
+                        :nudge-right="40"
+                        lazy
+                        transition="scale-transition"
+                        offset-y
+                        full-width
+                        max-width="290px"
+                        min-width="290px"
+                >
+                    <v-text-field
+                            slot="activator"
+                            v-model="afterDate"
+                            label="Runs After"
+                            readonly
+                    ></v-text-field>
+                    <v-date-picker v-model="afterDate" no-title @input="dateMenu2 = false"></v-date-picker>
+                </v-menu>
+            </v-flex>
+            <v-flex xs6>
+                <v-menu
+                        :close-on-content-click="false"
+                        v-model="dateMenu1"
+                        :nudge-right="40"
+                        lazy
+                        transition="scale-transition"
+                        offset-y
+                        full-width
+                        max-width="290px"
+                        min-width="290px"
+                >
+                    <v-text-field
+                            slot="activator"
+                            v-model="beforeDate"
+                            label="Runs Before"
+                            readonly
+                    ></v-text-field>
+                    <v-date-picker v-model="beforeDate" no-title @input="dateMenu1 = false"></v-date-picker>
+                </v-menu>
+            </v-flex>
+        </v-layout>
     </v-form>
 </template>
 
 <script>
     import axios from 'axios'
 
-    function getRecords(game) {
-        var gameId = game.id
-        return axios.get('https://www.speedrun.com/api/v1/games/' + gameId + '/records', {
-            params: {
-                'skip-empty': true,
-                'top': 1,
-                'scope': 'full-game',
-                'skip-empty': true
-            }
-        })
-        .then(function(response) {
-            var randomCategory = Math.floor(Math.random() * response.data.length)
-            return response.data;
-        })
-    }
-
-    function getRun(id) {
-        return axios.get('https://www.speedrun.com/api/v1/runs/' + id , {
-            params: {
-                'embed': 'players,category'
-            }
-        })
-        .then(function(response) {
-            return response.data;
-        })
-    }
-
-
-
     export default {
         mounted() {
-            console.log('Component mounted.')
+
         },
         data() {
             return {
                 autoPlay: false,
-                active: false
+                videoType: '0',
+                beforeDate: null,
+                afterDate: null,
+                dateFormatted: null,
+                dateMenu1: false,
+                dateMenu2: false,
+                minRunLength: null,
+                maxRunLength: null
             }
         },
         methods: {
@@ -65,11 +104,22 @@
                 .catch(function(response) {
 
                 })
+            },
+            formatDate (date) {
+                if (!date) return null
+                const [year, month, day] = date.split('-')
+                return `${month}/${day}/${year}`
             }
         },
         computed: {
             gameCount() {
                 return store.state.count
+            },
+            computedDateFormatted () {
+                return this.formatDate(this.date)
+            },
+            autoPlayLabel() {
+                return (this.autoPlay === true) ? 'On' : 'Off';
             }
         }
     }
