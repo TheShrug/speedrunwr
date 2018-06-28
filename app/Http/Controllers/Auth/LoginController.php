9e9e7a8551625981ab3564b082027b7afa220813
TheShrug <stewart.m.gordon@gmail.com>
2018-06-27 18:42:17 -0700

    Extending AuthenticatesUsers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,4 +39,34 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+	/**
+	 * Validate the user login request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return void
+	 */
+	protected function validateLogin(Request $request)
+	{
+		$this->validate($request, [
+			$this->userName() => 'required|string',
+			'password' => 'required|string',
+		]);
+	}
+
+	/**
+	 * Send the response after the user was authenticated.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	protected function sendLoginResponse(Request $request)
+	{
+		if(!$request->user()->verified) {
+			Auth::logout();
+			return json_encode(['message' => 'verifiedError', 'errorMessage' => 'Your email isn\'t validated, please check your inbox for a link to activate your account.']);
+		}
+		return json_encode(['message' => 'success', 'user' => $request->user(), 'likedRuns' => $request->user()->likedRuns()->orderBy('liked_run_user.created_at', 'desc')->get()]);
+
+	}
+
 }
