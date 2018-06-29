873a3518b0a16bed24e02738fcdf290b5a6fba5b
TheShrug <stewart.m.gordon@gmail.com>
2018-06-28 21:57:21 -0700

    show reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -38,6 +38,33 @@ class ResetPasswordController extends Controller
     }
 
 	/**
+	 * Reset the given user's password.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+	 */
+	public function reset(Request $request)
+	{
+		$this->validate($request, $this->rules(), $this->validationErrorMessages());
+
+		// Here we will attempt to reset the user's password. If it is successful we
+		// will update the password on an actual user model and persist it to the
+		// database. Otherwise we will parse the error and return the response.
+		$response = $this->broker()->reset(
+			$this->credentials($request), function ($user, $password) {
+			$this->resetPassword($user, $password);
+		}
+		);
+
+		// If the password was successfully reset, we will redirect the user back to
+		// the application's home authenticated view. If there is an error we can
+		// redirect them back to where they came from with their error message.
+		return $response == Password::PASSWORD_RESET
+			? $this->sendResetResponse($request, $response)
+			: $this->sendResetFailedResponse($request, $response);
+	}
+
+	/**
 	 * Get the response for a failed password reset.
 	 *
 	 * @param  \Illuminate\Http\Request  $request
