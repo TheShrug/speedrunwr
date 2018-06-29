392e8a9bd3f821d0ce1dc4e908cbf87bcc53da0e
TheShrug <stewart.m.gordon@gmail.com>
2018-06-28 21:59:55 -0700

    Password reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -38,6 +38,22 @@ class ResetPasswordController extends Controller
     }
 
 	/**
+	 * Display the password reset view for the given token.
+	 *
+	 * If no token is present, display the link request form.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  string|null  $token
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
+	public function showResetForm(Request $request, $token = null)
+	{
+		return view('auth.passwords.reset')->with(
+			['token' => $token, 'email' => $request->email]
+		);
+	}
+
+	/**
 	 * Reset the given user's password.
 	 *
 	 * @param  \Illuminate\Http\Request  $request
