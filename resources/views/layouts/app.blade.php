a01dd4cf978a39bdf509141f817e2bf8955fe388
TheShrug <stewart.m.gordon@gmail.com>
2018-07-29 11:40:47 -0700

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     @if (isset($meta))
         <title>Random Speedrun World Record Videos | Speedrun Web Randomizer</title>
         <meta name="description" content="{{ $meta['description'] }}">
